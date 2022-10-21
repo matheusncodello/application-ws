154ca06e87d8fe8f3afafef71bb195fcd1b38296
matheusncodello <matheus.ncodello@gmail.com>
2022-10-21 18:14:42 -0300

    feat: exception handler

---
+++ src/main/java/com/sptech/applicationws/controllers/dto/DefaultExceptionResponse.java
@@ -0,0 +1,28 @@
+package com.sptech.applicationws.controllers.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class DefaultExceptionResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private String message;
+    private String path;
+
+    public DefaultExceptionResponse(String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.message = message;
+        this.path = path;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public String getPath() {
+        return path;
+    }
+}
